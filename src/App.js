ba1c75ae663582afc4c973dd3c2be802f6d1c3a7
Robbie213 <sasnarinedeodat407@gmail.com>
2022-08-27 18:42:29 -0400

    Using State(useStateHook)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import Home from "./Home";
 import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = {name: "Yoshi", age: 30};
-  const link = "http://www.googl.com";
   return (
     <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+      <Navbar/>
+      <Home/>
       </div>
-    </div>
   );
 }
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,20 @@
-import React from "react";
+import { useState } from "react";
 
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("hello, ninjas", e);
+  // let name = "mario";
+  const [name, setName] = useState("mario");
+  const[age,setAge]=useState(25)
+
+  const handleClick = () => {
+    setName( "luigi");
+    setAge(30)
   };
-  const handleClickAgain = (name,e) => {
-    console.log("Hello" + name, e.target);
-  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
     </div>
   );
 };
