2201699b131bb8777041dce021e345cda5de5ac8
Robbie213 <sasnarinedeodat407@gmail.com>
2022-08-27 13:34:22 -0400

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("hello, ninjas", e);
+  };
+  const handleClickAgain = (name,e) => {
+    console.log("Hello" + name, e.target);
+  };
   return (
-      <div className='home'>
-          <h2>Homepage</h2>
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
